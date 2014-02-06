8f98327c9118535a254672cc751605c2a21e1c71
Hack Reactor Students <c.hughes87@gmail.com>
2014-02-05 17:28:37 -0800

    implements authentication with sha1 digest and password salt

--- db/schema.rb
+++ db/schema.rb
@@ -8,23 +8,31 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131022053000) do
+ActiveRecord::Schema.define(version: 20140205225831) do
 
-  create_table "clicks", :force => true do |t|
+  create_table "clicks", force: true do |t|
     t.integer  "link_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "links", :force => true do |t|
+  create_table "links", force: true do |t|
     t.string   "url"
     t.string   "code"
-    t.datetime "created_at",                :null => false
-    t.datetime "updated_at",                :null => false
-    t.integer  "visits",     :default => 0
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "visits",     default: 0
     t.string   "title"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "password_hash"
+    t.string   "password_salt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
--- shortly.rb
+++ shortly.rb
@@ -5,6 +5,7 @@ require 'digest/sha1'
 require 'pry'
 require 'uri'
 require 'open-uri'
+require 'digest/sha1'
 # require 'nokogiri'
 
 ###########################################################
@@ -28,6 +29,22 @@ end
 # turn off root element rendering in JSON
 ActiveRecord::Base.include_root_in_json = false
 
+# set up session 
+set :sessions => true
+register do
+  def auth (type)
+    condition do
+      if (session[:user_id])
+        user = User.find_by_id(session[:user_id])
+        id = user.id unless !user
+      end
+      redirect "/login" unless session[:user_id] && session[:user_id] == id
+      ##redirect "/login" unless session[:user_id] && session[:user_id] == User.find(session[:user_id]).id
+    end
+  end
+end
+
+
 ###########################################################
 # Models
 ###########################################################
@@ -45,26 +62,61 @@ class Link < ActiveRecord::Base
     end
 end
 
+class User < ActiveRecord::Base
+end
+
 class Click < ActiveRecord::Base
     belongs_to :link, counter_cache: :visits
 end
 
+loggedInUser = 0
 ###########################################################
 # Routes
 ###########################################################
-
-get '/' do
+get '/', :auth => :user do
     erb :index
 end
 
-get '/links' do
+get '/login' do
+  erb :login
+  # return if loggedInUser == 0
+  # session[:username] = User.find( loggedInUser ).username
+  # session[:user_id] = loggedInUser # User.find_by_username('bob').id
+  # "Hello #{session[:username]}"
+end
+
+get '/links', :auth => :user do
     links = Link.order("created_at DESC")
     links.map { |link|
         link.as_json.merge(base_url: request.base_url)
     }.to_json
 end
 
-post '/links' do
+post '/login' do
+  user = User.find_by_username(params[:username])
+  if !user || user.password_hash != Digest::SHA1.hexdigest(params[:password]+user.password_salt)[0,63]
+    session[:user_id] = nil
+    redirect to '/login'
+  else
+    session[:user_id] = user.id
+    redirect to '/'
+  end
+end
+
+post '/user' do
+  if params[:password] != params[:password_confirm]
+    puts "Password #{params[:password]} doesn't match #{params[:password_confirm]}"
+    return "passwords don't match!"#redirect to '/login' # TODO display message instead?
+  end
+  username = params[:username]
+  password_salt = Digest::SHA1.hexdigest( Random.rand( 10**10 ).to_s )
+  password_hash = Digest::SHA1.hexdigest(params[:password]+ password_salt)[0,63]
+  User.create(username: username, password_hash: password_hash, password_salt: password_salt)
+  session[:user_id] = User.find_by_username( username ).id
+  redirect to '/'
+end
+
+post '/links', :auth => :user do
     data = JSON.parse request.body.read
     uri = URI(data['url'])
     raise Sinatra::NotFound unless uri.absolute?
@@ -73,7 +125,7 @@ post '/links' do
     link.as_json.merge(base_url: request.base_url).to_json
 end
 
-get '/:url' do
+get '/:url', :auth => :user do
     link = Link.find_by_code params[:url]
     raise Sinatra::NotFound if link.nil?
     link.clicks.create!
